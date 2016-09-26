1921d4653eb979b63db7691d78bb406148cbcee2
Rosie <ariel.highsmith@gmail.com>
2016-09-26 15:24:11 -0700

    Mark as Complete function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926051926) do
+ActiveRecord::Schema.define(version: 20160926220155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160926051926) do
     t.integer  "todo_list_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.datetime "completed_at"
     t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id", using: :btree
   end
 
